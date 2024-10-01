9e15c6c8ca8a9481a66a886fd0005a986bc21783
tsuminetic <diensanc@gmail.com>
2024-10-01 23:03:42 +0545

    specify port and address

--- app.py
+++ app.py
@@ -262,4 +262,4 @@ def disease_prediction():
 
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0", port=5000)
